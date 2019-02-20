6648c112034725e32f43702d1258542c51fed7f2
cpusoft <cpusoft@live.com>
2019-02-20 10:57:30 +0800

    add

---
+++ src/main/http5/http5.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	_ "encoding/json"
+	_ "fmt"
+	"log"
+	"net"
+	"net/http"
+	_ "strings"
+	_ "sync"
+
+	"github.com/ant0ine/go-json-rest/rest"
+)
+
+func main() {
+	/*
+		api := rest.NewApi()
+		api.Use(rest.DefaultDevStack...)
+		api.SetApp(rest.AppSimple(func(w rest.ResponseWriter, r *rest.Request) {
+			w.WriteJson(map[string]string{"Body": "Hello World!"})
+		}))
+		log.Fatal(http.ListenAndServe(":8080", api.MakeHandler()))
+	*/
+
+	go func() {
+		api1 := rest.NewApi()
+		api1.Use(rest.DefaultDevStack...)
+		router1, err := rest.MakeRouter(
+			rest.Get("/lookup/#host", Lookup),
+		)
+		if err != nil {
+			log.Fatal(err)
+		}
+		api1.SetApp(router1)
+		http.ListenAndServe(":8080", api1.MakeHandler())
+	}()
+
+	go func() {
+		api2 := rest.NewApi()
+		api2.Use(rest.DefaultDevStack...)
+		router2, err := rest.MakeRouter(
+			rest.Get("/admin/fetchallroa", FetchAllRoa),
+		)
+		if err != nil {
+			log.Fatal(err)
+		}
+		api2.SetApp(router2)
+		http.ListenAndServe(":8081", api2.MakeHandler())
+	}()
+	go func() {
+		//go run "D:\Program Files\Go\src\crypto\tls\generate_cert.go" --host localhost
+		certFile := `E:\Go\go-study\src\main\http5\cert_gobuild.pem`
+		keyFile := `E:\Go\go-study\src\main\http5\key_gobuild.pem`
+
+		api3 := rest.NewApi()
+		//statusMw = &rest.StatusMiddleware{}
+		api3.Use(statusMw)
+		api3.Use(rest.DefaultDevStack...)
+		router3, err := rest.MakeRouter(
+			rest.Get("/https/status", Status),
+		)
+		if err != nil {
+			log.Fatal(err)
+		}
+		api3.SetApp(router3)
+		http.ListenAndServeTLS(":8443", certFile, keyFile, api3.MakeHandler())
+	}()
+	select {}
+
+}
+
+func Lookup(w rest.ResponseWriter, req *rest.Request) {
+	log.Print(req.PathParam("host"))
+	ip, err := net.LookupIP(req.PathParam("host"))
+	if err != nil {
+		rest.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.WriteJson(&ip)
+}
+func FetchAllRoa(w rest.ResponseWriter, req *rest.Request) {
+	w.WriteJson("ok")
+}
+
+var statusMw = &rest.StatusMiddleware{}
+
+func Status(w rest.ResponseWriter, req *rest.Request) {
+	w.WriteJson(statusMw.GetStatus())
+}
--- src/main/test/test.go
+++ src/main/test/test.go
@@ -66,23 +66,69 @@ func DecodeGeneralizedTime(data []byte) (ret string, err error) {
 	return year + "-" + month + "-" + day + " " + hour + ":" + minute + ":" + second + z, nil
 }
 
-func main() {
-	data := []byte{0x31, 0x38, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-		0x34, 0x34, 0x35, 0x36, 0x5A, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}
-	pos := GetIndexLast0000(data)
-	fmt.Println(pos)
-	/*
-		data := []byte{0x31, 0x38, 0x31, 0x30, 0x30, 0x33, 0x30, 0x37, 0x34, 0x34, 0x35, 0x36, 0x5A, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}
-		pos := GetTopHierarchyFor00(data)
-		fmt.Println(pos)
-		/*
-			data = []byte{0x31, 0x38, 0x31, 0x30, 0x30, 0x33, 0x30, 0x37, 0x34, 0x34, 0x35, 0x36, 0x5A}
-			ret, _ := DecodeUTCTime(data)
-			fmt.Println(ret)
+func PrintBytes(name string, buf *[]byte, indent string) {
+	if len(name) > 0 {
+		fmt.Print(name)
+	}
+	data_lines := make([]string, (len(*buf)/30)+1)
 
-			data = []byte{0x32, 0x30, 0x31, 0x38, 0x30, 0x36, 0x32, 0x38, 0x32, 0x30, 0x33, 0x31, 0x31, 0x36, 0x5A}
-			ret, _ = DecodeGeneralizedTime(data)
-			fmt.Println(ret)
+	for i, b := range *buf {
+		data_lines[i/30] += fmt.Sprintf("%02x ", b)
+	}
+
+	for i := 0; i < len(data_lines); i++ {
+		//belogs.Debug(indent + data_lines[i])
+		fmt.Print(indent + data_lines[i] + "\n\n")
+	}
+}
+func InversionCount(a *[]int) int {
+	if len(*a) <= 1 {
+		return 0
+	}
+	mid := len(*a) / 2
+	left := (*a)[:mid]
+	right := (*a)[mid:]
+	leftCount := InversionCount(&left)   //not being sorted properly due to reference issues
+	rightCount := InversionCount(&right) //not being sorted properly due to reference issues
+
+	res := make([]int, 0, len(right)+len(left)) //temp slice to hold the sorted left side and right side
+
+	iCount := 1 //mergeCount(left, right, &res)
+
+	*a = res
+	fmt.Println(a) //a in the end is not sorted properly for most cases
+	return iCount + leftCount + rightCount
+}
+func FmtBytes(buf *[]byte) {
+	fmt.Printf("%p\r\n", buf)
+}
+func main() {
+	data1 := []byte("sss")
+	data := []byte{0x31, 0x38, 0x00, 0x31, 0x38, 0x00}
+	fmt.Printf("%p\r\n", &data1)
+	FmtBytes(&data1)
+	left := data1[:]
+	FmtBytes(&left)
+
+	fmt.Printf("%p\r\n", &data)
+	FmtBytes(&data)
+	left = data[:]
+	FmtBytes(&left)
+	/*
+		pos := GetIndexLast0000(data)
+		fmt.Println(pos)
+
+			data := []byte{0x31, 0x38, 0x31, 0x30, 0x30, 0x33, 0x30, 0x37, 0x34, 0x34, 0x35, 0x36, 0x5A, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}
+			pos := GetTopHierarchyFor00(data)
+			fmt.Println(pos)
+			/*
+				data = []byte{0x31, 0x38, 0x31, 0x30, 0x30, 0x33, 0x30, 0x37, 0x34, 0x34, 0x35, 0x36, 0x5A}
+				ret, _ := DecodeUTCTime(data)
+				fmt.Println(ret)
+
+				data = []byte{0x32, 0x30, 0x31, 0x38, 0x30, 0x36, 0x32, 0x38, 0x32, 0x30, 0x33, 0x31, 0x31, 0x36, 0x5A}
+				ret, _ = DecodeGeneralizedTime(data)
+				fmt.Println(ret)
 	*/
 
 }
