c219672af855ed227a50f342760f11bfb5d8bc60
cpusoft <cpusoft@live.com>
2019-11-12 10:47:46 +0800

    add

--- src/main/xormdb/xormdb1.go
+++ src/main/xormdb/xormdb1.go
@@ -3,7 +3,7 @@ package main
 import (
 	_ "database/sql"
 	"fmt"
-	"time"
+	_ "time"
 
 	belogs "github.com/astaxie/beego/logs"
 	_ "github.com/go-sql-driver/mysql"
@@ -75,34 +75,57 @@ func main() {
 		fmt.Println(id)
 	*/
 	//lab_rpki_sync_log_file
-	type LabRpkiSyncLogFile struct {
-		Id        uint64 `json:"id" xorm:"pk autoincr"`
-		SyncLogId uint64 `json:"syncLogId" xorm:"syncLogId int"`
-		//cer/roa/mft/crl, not dot
-		FileType string `json:"fileType" xorm:"fileType varchar(16)"`
-		//sync time for every file
-		SyncTime                time.Time `json:"syncTime" xorm:"syncTime datetime"`
-		Ski                     string    `json:"ski" xorm:"ski varchar(128)"`
-		Aki                     string    `json:"aki" xorm:"aki varchar(128)"`
-		FilePath                string    `json:"filePath" xorm:"filePath varchar(512)"`
-		FileName                string    `json:"fileName" xorm:"fileName varchar(128)"`
-		ParseValidateResultJson string    `json:"syncJson" xorm:"syncJson json"`
-		JsonAll                 string    `json:"jsonAll" xorm:"jsonAll json"`
-		LastJsonAll             string    `json:"jsonAll" xorm:"lastJsonAll json"`
-		FileHash                string    `json:"fileHash" xorm:"fileHash varchar(512)"`
-		//add/update/del
-		SyncType string `json:"syncType" xorm:"syncType varchar(16)"`
-		//LabRpkiSyncLogFileState:
-		State string `json:"state" xorm:"state json"`
-	}
-	syncLogFiles := make([]LabRpkiSyncLogFile, 0)
-	//err = xormdb.XormEngine.Table(&syncLogFile).Where("syncLogId = ?", syncLog.Id).And("state->>'$.updateCertTable'=?", "notYet").Asc("id").Find(&syncLogFiles)
+	/*
+		type LabRpkiSyncLogFile struct {
+			Id        uint64 `json:"id" xorm:"pk autoincr"`
+			SyncLogId uint64 `json:"syncLogId" xorm:"syncLogId int"`
+			//cer/roa/mft/crl, not dot
+			FileType string `json:"fileType" xorm:"fileType varchar(16)"`
+			//sync time for every file
+			SyncTime                time.Time `json:"syncTime" xorm:"syncTime datetime"`
+			Ski                     string    `json:"ski" xorm:"ski varchar(128)"`
+			Aki                     string    `json:"aki" xorm:"aki varchar(128)"`
+			FilePath                string    `json:"filePath" xorm:"filePath varchar(512)"`
+			FileName                string    `json:"fileName" xorm:"fileName varchar(128)"`
+			ParseValidateResultJson string    `json:"syncJson" xorm:"syncJson json"`
+			JsonAll                 string    `json:"jsonAll" xorm:"jsonAll json"`
+			LastJsonAll             string    `json:"jsonAll" xorm:"lastJsonAll json"`
+			FileHash                string    `json:"fileHash" xorm:"fileHash varchar(512)"`
+			//add/update/del
+			SyncType string `json:"syncType" xorm:"syncType varchar(16)"`
+			//LabRpkiSyncLogFileState:
+			State string `json:"state" xorm:"state json"`
+		}
+		syncLogFiles := make([]LabRpkiSyncLogFile, 0)
+		//err = xormdb.XormEngine.Table(&syncLogFile).Where("syncLogId = ?", syncLog.Id).And("state->>'$.updateCertTable'=?", "notYet").Asc("id").Find(&syncLogFiles)
 
-	err = engine.Table("lab_rpki_sync_log_file").Select("id,filePath, fileName, fileType, syncType").
-		Where("state->>'$.updateCertTable'=?", "notYet").And("syncLogId=?", 18).OrderBy("id").Find(&syncLogFiles)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(len(syncLogFiles))
+		err = engine.Table("lab_rpki_sync_log_file").Select("id,filePath, fileName, fileType, syncType").
+			Where("state->>'$.updateCertTable'=?", "notYet").And("syncLogId=?", 18).OrderBy("id").Find(&syncLogFiles)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println(len(syncLogFiles))
+	*/
+
+	/*
+			select filePath from lab_rpki_cer where fileName='eca135ce-a78f-437e-a32f-4714e9a373d2.cer'
+		union
+		select filePath from lab_rpki_crl where fileName='eca135ce-a78f-437e-a32f-4714e9a373d2.cer'
+		union
+		select filePath from lab_rpki_mft where fileName='eca135ce-a78f-437e-a32f-4714e9a373d2.cer'
+		union
+		select filePath from lab_rpki_mft where fileName='eca135ce-a78f-437e-a32f-4714e9a373d2.cer'
+	*/
+	sqls := `
+	    select filePath from lab_rpki_cer where fileName='eca135ce-a78f-437e-a32f-4714e9a373d2.cer'
+		union
+		select filePath from lab_rpki_crl where fileName='eca135ce-a78f-437e-a32f-4714e9a373d2.cer'
+		union
+		select filePath from lab_rpki_mft where fileName='eca135ce-a78f-437e-a32f-4714e9a373d2.cer'
+		union
+		select filePath from lab_rpki_mft where fileName='eca135ce-a78f-437e-a32f-4714e9a373d2.cer'`
+	var filePath string
+	has, err := engine.SQL(sqls).Get(&filePath)
+	fmt.Println(filePath, has, err)
 }
