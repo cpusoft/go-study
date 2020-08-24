77c7d0f5aa7a284ca37485d83512cac26db0bcc3
cpusoft <cpusoft@live.com>
2020-08-24 10:27:25 +0800

    add

--- src/asnmft1/asnee.go
+++ src/asnmft1/asnee.go
@@ -3,7 +3,7 @@ package main
 import (
 	"encoding/asn1"
 	"fmt"
-	_ "time"
+	"time"
 
 	"github.com/cpusoft/goutil/convert"
 )
@@ -24,9 +24,9 @@ type SignSha256 struct {
 }
 type OidOid struct {
 	Oid  asn1.ObjectIdentifier
-	Oids Oids `asn1:"tag:17"`
+	Oids Oids
 }
-type Oids []asn1.RawValue
+type Oids []interface{} //asn1.RawValue
 type OidTime struct {
 	Oid       asn1.ObjectIdentifier
 	SignTimes []asn1.RawValue //[]time.Time `asn:"utc"`
@@ -35,6 +35,66 @@ type Sign struct {
 	OidOid1 OidOid
 	OidTime asn1.RawValue //OidTime
 	OidOid2 asn1.RawValue //OidOid
+}
+
+func GetOidSetBytes(value []byte) (oid asn1.ObjectIdentifier, b []byte, err error) {
+	oid, raw, err := GetOidSetValue(value)
+	if err != nil {
+		fmt.Println("GetOidSetBytes(): GetOidSetValue fail:", err)
+		return
+	}
+
+	_, err = asn1.Unmarshal(raw.Bytes, &b)
+	if err != nil {
+		fmt.Println("GetOidSetBytes(): Unmarshal fail:", err)
+		return
+	}
+	return
+}
+
+func GetOidSetOid(value []byte) (oid asn1.ObjectIdentifier, oid1 asn1.ObjectIdentifier, err error) {
+	oid, raw, err := GetOidSetValue(value)
+	if err != nil {
+		fmt.Println("GetOidSetOid(): GetOidSetValue fail:", err)
+		return
+	}
+	oid1 = asn1.ObjectIdentifier{}
+	_, err = asn1.Unmarshal(raw.Bytes, &oid1)
+	if err != nil {
+		fmt.Println("GetOidSetOid(): Unmarshal fail:", err)
+		return
+	}
+	return
+}
+
+func GetOidSetTime(value []byte) (oid asn1.ObjectIdentifier, time time.Time, err error) {
+	oid, raw, err := GetOidSetValue(value)
+	if err != nil {
+
+		return
+	}
+	//Tm{}
+	_, err = asn1.Unmarshal(raw.Bytes, &time)
+	if err != nil {
+		return
+	}
+	return
+}
+
+func GetOidSetValue(value []byte) (oid asn1.ObjectIdentifier, raw asn1.RawValue, err error) {
+	oid = asn1.ObjectIdentifier{}
+	rawRest, err := asn1.Unmarshal(value, &oid)
+	if err != nil {
+		fmt.Println("GetOidSetValue(): Unmarshal oid fail:", err)
+		return
+	}
+	raw = asn1.RawValue{}
+	_, err = asn1.Unmarshal(rawRest, &raw)
+	if err != nil {
+		fmt.Println("GetOidSetValue(): Unmarshal raw fail:", err)
+		return
+	}
+	return
 }
 
 func GetSignedData(value []byte) (signedData SignedData, sign Sign, err error) {
@@ -46,13 +106,26 @@ func GetSignedData(value []byte) (signedData SignedData, sign Sign, err error) {
 	raw := asn1.RawValue{}
 	rest, err := asn1.Unmarshal(signedData.Sign.Bytes, &raw)
 	fmt.Println("rest::", len(rest))
+	/*
+		oid := asn1.ObjectIdentifier{}
+		rawRest, err := asn1.Unmarshal(raw.Bytes, &oid)
+		raw = asn1.RawValue{}
+		_, err = asn1.Unmarshal(rawRest, &raw)
+		oid = asn1.ObjectIdentifier{}
+		_, err = asn1.Unmarshal(raw.Bytes, &oid)
+		fmt.Println("oid:", oid, err)
+	*/
+	oid, oid1, err := GetOidSetOid(raw.Bytes)
+	fmt.Println("oid:", oid, oid1, err)
 
-	oid := asn1.ObjectIdentifier{}
-	rawRest, err := asn1.Unmarshal(raw.Bytes, &oid)
 	raw = asn1.RawValue{}
-	_, err = asn1.Unmarshal(rawRest, &raw)
-	oid = asn1.ObjectIdentifier{}
-	_, err = asn1.Unmarshal(raw.Bytes, &oid)
-	fmt.Println("oid:", oid, err)
+	rest, err = asn1.Unmarshal(rest, &raw)
+	oid, time, err := GetOidSetTime(raw.Bytes)
+	fmt.Println("time:", oid, time, err)
+
+	raw = asn1.RawValue{}
+	rest, err = asn1.Unmarshal(rest, &raw)
+	oid, b, err := GetOidSetBytes(raw.Bytes)
+	fmt.Println("[]byte:", oid, convert.Bytes2String(b), err)
 	return signedData, sign, nil
 }
