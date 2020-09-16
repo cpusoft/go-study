164911425d6a1354547351baa803e987cb171185
cpusoft <cpusoft@live.com>
2020-09-16 15:51:22 +0800

    add

--- src/asnroa2/asnroa2.go
+++ src/asnroa2/asnroa2.go
@@ -4,14 +4,14 @@ import (
 	"encoding/asn1"
 	"fmt"
 
-	"github.com/cpusoft/goutil/convert"
+	_ "github.com/cpusoft/goutil/convert"
 	"github.com/cpusoft/goutil/fileutil"
 	"github.com/cpusoft/goutil/jsonutil"
 )
 
 type CertificateList struct {
 	Oid  asn1.ObjectIdentifier
-	Seqs []asn1.RawValue `asn1:"optional,explicit,default:0,tag:0""`
+	Seqs asn1.RawValue `asn1:"optional,explicit,default:0,tag:0""`
 }
 
 // asID as in rfc6482
@@ -45,38 +45,38 @@ func main() {
 		return
 	}
 	certificate := CertificateList{}
-	asn1.Unmarshal(b, &certificate)
-	fmt.Println("certificate:", jsonutil.MarshallJsonIndent(certificate))
+	rest, err := asn1.Unmarshal(b, &certificate)
+	fmt.Println("certificate:", jsonutil.MarshallJsonIndent(certificate), len(rest), err)
+	/*
+		// int 3 ???
+		fmt.Println("seqs[0]", convert.Bytes2Uint64(certificate.Seqs[0].Bytes), "\n")
 
-	// int 3 ???
-	fmt.Println("seqs[0]", convert.Bytes2Uint64(certificate.Seqs[0].Bytes), "\n")
+		// sha256
+		sha256 := Sha256{}
+		asn1.Unmarshal(certificate.Seqs[1].Bytes, &sha256)
+		fmt.Println("seqs[1]:Oids:", jsonutil.MarshallJsonIndent(sha256.Oid))
 
-	// sha256
-	sha256 := Sha256{}
-	asn1.Unmarshal(certificate.Seqs[1].Bytes, &sha256)
-	fmt.Println("seqs[1]:Oids:", jsonutil.MarshallJsonIndent(sha256.Oid))
+		// roa
+		oid1 := asn1.ObjectIdentifier{} //make([]asn1.RawValue, 0)
+		reset, err := asn1.Unmarshal(certificate.Seqs[2].Bytes, &oid1)
+		fmt.Println("seqs[2]:oid:", jsonutil.MarshallJsonIndent(oid1), err)
 
-	// roa
-	oid1 := asn1.ObjectIdentifier{} //make([]asn1.RawValue, 0)
-	reset, err := asn1.Unmarshal(certificate.Seqs[2].Bytes, &oid1)
-	fmt.Println("seqs[2]:oid:", jsonutil.MarshallJsonIndent(oid1), err)
+		raw := asn1.RawValue{}
+		_, err = asn1.Unmarshal(reset, &raw)
+		//fmt.Println("seqs[2]:", convert.PrintBytes(raw.Bytes, 8))
 
-	raw := asn1.RawValue{}
-	_, err = asn1.Unmarshal(reset, &raw)
-	//fmt.Println("seqs[2]:", convert.PrintBytes(raw.Bytes, 8))
+		raw1 := asn1.RawValue{}
+		_, err = asn1.Unmarshal(raw.Bytes, &raw1)
+		//fmt.Println("seqs[2]:", convert.PrintBytes(raw1.Bytes, 8))
 
-	raw1 := asn1.RawValue{}
-	_, err = asn1.Unmarshal(raw.Bytes, &raw1)
-	//fmt.Println("seqs[2]:", convert.PrintBytes(raw1.Bytes, 8))
+		routeOriginAttestation := RouteOriginAttestation{}
+		_, err = asn1.Unmarshal(raw1.Bytes, &routeOriginAttestation)
+		fmt.Println("seqs[2]:roa:", jsonutil.MarshallJsonIndent(routeOriginAttestation))
 
-	routeOriginAttestation := RouteOriginAttestation{}
-	_, err = asn1.Unmarshal(raw1.Bytes, &routeOriginAttestation)
-	fmt.Println("seqs[2]:roa:", jsonutil.MarshallJsonIndent(routeOriginAttestation))
-
-	// ee cer
-	cer := Certificate{}
-	asn1.Unmarshal(certificate.Seqs[3].Bytes, &cer)
-	fmt.Println("certificate:", jsonutil.MarshallJsonIndent(cer))
-	fmt.Println(len(cer.TBSCertificate.Extensions))
-
+		// ee cer
+		cer := Certificate{}
+		asn1.Unmarshal(certificate.Seqs[3].Bytes, &cer)
+		fmt.Println("certificate:", jsonutil.MarshallJsonIndent(cer))
+		fmt.Println(len(cer.TBSCertificate.Extensions))
+	*/
 }
