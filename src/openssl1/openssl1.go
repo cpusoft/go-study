239267bacc3edd895fe43dcd742ac0457ed797e7
shao <shaoqing@zdns.cn>
2024-10-29 17:01:26 +0800

    openssl

--- src/openssl1/openssl1.go
+++ src/openssl1/openssl1.go
@@ -2,21 +2,13 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"os/exec"
-
-	"github.com/cpusoft/goutil/belogs"
 )
 
 func main() {
-	cmd := exec.Command("openssl", "version")
-	ldLibraryPath := `/home/openssl/openssl/lib64`
-	path := `/home/openssl/openssl/bin:$PATH`
-	if len(ldLibraryPath) > 0 && len(path) > 0 {
-		cmd.Env = append(os.Environ(), "LD_LIBRARY_PATH="+ldLibraryPath)
-		cmd.Env = append(os.Environ(), "PATH="+path)
-		belogs.Debug("main(): ldLibraryPath:", ldLibraryPath, "  path:", path)
-	}
+	opensslCmd := `/home/openssl/openssl/bin/openssl`
+	cmd := exec.Command(opensslCmd, "version")
+
 	output, err := cmd.CombinedOutput()
-	fmt.Println(output, err)
+	fmt.Println(string(output), err)
 }
